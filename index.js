2d330875d82e28e9a57e412d228b6c2566ca6258
gevorg-harutyunyan <harutyunyangevorg01@gmail.com>
2022-08-05 20:43:39 +0400

    my first commit

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-let express = require('express');
-let app = express();
-let port = process.env.PORT || 3000;
-let fs = require('fs');
-let gamesInfo = JSON.parse(fs.readFileSync('games.json', 'utf8'));
-console.log( "You have " + gamesInfo.length + " games." );
+let express = require("express")
+let app = express()
+let port = process.env.PORT || 3000
+let fs = require("fs")
+let gamesInfo = JSON.parse(fs.readFileSync("games.json", "utf8"))
+console.log("You have " + gamesInfo.length + " games.")
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs")
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname))
 
-app.get('/', (req, res) => {
-    res.render('index', { gamesInfo: gamesInfo, index_url_code: req.query  });
-    
-});
+app.get("/", (req, res) => {
+  res.render("index", { gamesInfo: gamesInfo, index_url_code: req.query })
+})
 
-app.get('/game', (req, res) => {
-  res.render('gamepage', { gamesInfo: gamesInfo, index_url_code: req.query });
-   
-});
- 
+app.get("/game", (req, res) => {
+  res.render("gamepage", { gamesInfo: gamesInfo, index_url_code: req.query })
+})
 
-
-     
-app.listen(port);
+app.listen(port)
